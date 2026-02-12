a8b1d4def50b9d13c432203276794716ac12756e
Kim Chang mo <kimchangmo@Kimui-MacBookAir.local>
2026-02-12 21:59:47 +0900

    feat: Add mobile support (Android/iOS) via Capacitor

---
+++ android/app/src/main/java/com/gart76/simplememo/MainActivity.java
@@ -0,0 +1,5 @@
+package com.gart76.simplememo;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.gart76.simplememo',
+  appName: 'SimpleMemo',
+  webDir: 'dist'
+};
+
+export default config;
